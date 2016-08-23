9001d20a335a95b2e94b095826815a815a48e627
Rex R Kelly <rexfordkelly@gmail.com>
2016-08-23 15:16:24 -0700

    init

---
+++ 00b_operators-assignment.js
@@ -0,0 +1,17 @@
+/*
+
+
+	An assignment operator assigns a value to its left operand 
+	based on the value of its right operand.
+
+	GOTO: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Assignment_Operators	
+
+	look at the following assignment operators:
+
+	Assignment							x = y			x = y
+	Addition assignment					x += y			x = x + y
+	Subtraction assignment				x -= y			x = x - y
+	Multiplication assignment			x *= y			x = x * y
+	Division assignment					x /= y			x = x / y
+	Remainder assignment				x %= y			x = x % y
+
